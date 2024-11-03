57f4100cb756de435af1fad1f476dfd831451dc3
Nathanael Hermanto <nathanael887@gmail.com>
2024-11-03 01:42:34 +0100

    first commit

---
+++ src/main/java/com/nathancorp/pabrik/PabrikApplication.java
@@ -0,0 +1,13 @@
+package com.nathancorp.pabrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabrikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabrikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathancorp/pabrik/PabrikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathancorp.pabrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabrikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
