ca8a335613413f65d308c71ad15d1dcd3eb1f9e1
Nathanael Hermanto <nathanael887@gmail.com>
2024-11-05 15:31:03 +0100

    dbstuff

--- src/test/java/com/nathancorp/pabrik/PabrikApplicationTests.java
+++ src/test/java/com/nathancorp/pabrik/PabrikApplicationTests.java
@@ -3,7 +3,7 @@ package com.nathancorp.pabrik;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.config.location=classpath:/application-test.properties")
+@SpringBootTest(properties = "PABRIK_SECRET_KEY=RWl2V2iChIEyicARsgOAnIJ2R/nYqFTqaZS21BZ3Wns=")
 class PabrikApplicationTests {
 
 	@Test
